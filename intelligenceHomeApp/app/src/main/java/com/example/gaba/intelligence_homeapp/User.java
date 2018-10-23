31ae2a39d004d3fa6c3e149afcfd68327941d860
Gabrielle Naubert <gnaubert@xelerance.com>
2018-10-23 16:20:24 -0400

    Add getRole() to User class

--- intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/User.java
+++ intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/User.java
@@ -7,13 +7,17 @@ public class User {
     private String role;
 
     public User() {
-        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+
     }
 
     public User(String username, String password, String role) {
         this.username = username;
         this.password = password;
         this.role = role;
+    }
+
+    public String getRole(){
+        return role;
     }
 
 }

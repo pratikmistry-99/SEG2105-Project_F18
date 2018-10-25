e9433404a108b1c1d11131bd5539c00f9ae5fd7e
Nikita Bliumkin <nbliu026@uottawa.ca>
2018-10-25 00:30:28 -0400

    Welcome_Screen

--- intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/WelcomeScreen.java
+++ intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/WelcomeScreen.java
@@ -8,16 +8,19 @@ import android.widget.TextView;
 public class WelcomeScreen extends AppCompatActivity {
 
     TextView userNameDisplay;
+    TextView userRoleDisplay;
     String str;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-
-        userNameDisplay = findViewById(R.id.userDisplay);
+        userRoleDisplay = findViewById(R.id.userRoleDisplay);
+        userNameDisplay = findViewById(R.id.userNameDisplay);
         str = getIntent().getExtras().getString("value");
         userNameDisplay.setText(str);
+        userRoleDisplay.setText(Users.getUser(str).getRole());
 
     }
 }

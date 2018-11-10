5b0a71323ea4e3f295bfb70a03dd3f0048f02cee
Saabiqa <schow043@uottawa.ca>
2018-11-10 17:51:22 -0500

    temp

---
+++ intelligenceHomeApp/app/src/test/java/com/example/gaba/intelligence_homeapp/AdminFunctionTest.java
@@ -0,0 +1,22 @@
+package com.example.gaba.intelligence_homeapp;
+
+import org.junit.Test;
+
+public class AdminFunctionTest {
+
+    @Test
+    public void addService() throws Exception{
+
+    }
+
+    @Test
+    public void editService() throws Exception{
+
+    }
+
+    @Test
+    public void deleteService() throws Exception{
+
+    }
+}
+

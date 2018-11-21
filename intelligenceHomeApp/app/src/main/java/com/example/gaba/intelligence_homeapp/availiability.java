8285a644062724332c67132f29231ea12c006c82
Rithik Sowdermett <rsowd046@uottawa.ca>
2018-11-21 17:39:43 -0500

    some minor changes to the availability screen

--- intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/availiability.java
+++ intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/availiability.java
@@ -1,7 +1,10 @@
 package com.example.gaba.intelligence_homeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class availiability extends AppCompatActivity {
 
@@ -10,4 +13,10 @@ public class availiability extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availiability);
     }
+
+    public void saveBtn(View view){
+        
+        finish();
+    }
+
 }

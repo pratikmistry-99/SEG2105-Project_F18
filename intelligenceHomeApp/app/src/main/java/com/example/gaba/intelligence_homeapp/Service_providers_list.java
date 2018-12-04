b57ac48dac06c97551f5b623062a17645cbe2ab9
pratikmistry-99 <pmist017@uottawa.ca>
2018-12-03 22:02:48 -0500

    added ratings button functionality

--- intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/Service_providers_list.java
+++ intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/Service_providers_list.java
@@ -2,11 +2,14 @@ package com.example.gaba.intelligence_homeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -58,4 +61,31 @@ public class Service_providers_list extends AppCompatActivity{
         startActivityForResult(intent, 0);
     }
 
-}
+    public void viewRating(View view){
+        showRatingSelectDialog();
+    }
+
+    private void showRatingSelectDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.activity_rating_select_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        final Button btnSearch = (Button) dialogView.findViewById(R.id.btnSearch);
+        final EditText editRating = (EditText) dialogView.findViewById(R.id.editRating);
+
+        dialogBuilder.setTitle("Rating Search");
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //Need to make a method that makes a list based on the given rating and displays it
+                b.dismiss();
+            }
+
+        });
+    }
+
+
+    }
---
+++ intelligenceHomeApp/app/src/main/java/com/example/gaba/intelligence_homeapp/rating_select_dialog.java
@@ -0,0 +1,13 @@
+package com.example.gaba.intelligence_homeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rating_select_dialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating_select_dialog);
+    }
+}
